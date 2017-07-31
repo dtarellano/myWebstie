9c2f77abecd94e54248f46a5e04cf83d6cf88ce8
David Arellano <dtarellano1@gmail.com>
2017-07-30 22:24:43 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+     {{ test }}
+  </div>
+</template>
+
+<script>
+export default {
+   name: "app",
+   data() {
+      return {
+         test: "Test"
+      };
+   }
+};
+</script>
+
+<style lang="scss">
+
+</style>
